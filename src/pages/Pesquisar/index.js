dcb66bcedd0b363b801c5d56c74374d61d26119a
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-03-07 15:42:13 -0300

    pesquisar v1

--- src/pages/Pesquisar/index.js
+++ src/pages/Pesquisar/index.js
@@ -41,9 +41,176 @@ export default function Cadastro() {
 						className="d-flex py-2  flex-column flex-md-row justify-content-between"
 						style={{flexWrap: 'wrap' }}
 					>
+
+
 						<div
 							className="custom-card mx-5 mb-3 flex-column"
-							style={{ Width: 350, display: 'flex', flex: 1 }}
+							style={{ display: 'flex', flex: 'auto' }}
+						>
+							<Link to="" style={{ maxWidth: '100px', alignSelf:'center', marginTop:10 }}>
+								<img
+									src="https://is1-ssl.mzstatic.com/image/thumb/Podcasts123/v4/6b/06/fd/6b06fdbd-07b0-032e-7f4d-ea7c40995278/mza_3169076889313867971.jpg/400x400bb.jpg"
+									style={{ maxHeight: '100px', maxWidth: '100px' }}
+									className="img-thumb"
+									alt="#"
+								/>
+							</Link>
+							<div style={{ flex: 1, padding: '5px 10px', alignSelf:'center' }}>
+								<Link to="#" style={{textAlign:'center'}}>
+									<p
+										style={{
+											fontSize: '1rem',
+											marginBottom: 0,
+											fontFamily: 'inherit',
+											fontWeight: 500,
+											lineHeight: 1.2
+										}}
+									>
+										Nerdcast
+									</p>
+								</Link>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+							</div>
+						</div>
+
+
+
+
+
+
+
+						<div
+							className="custom-card mx-5 mb-3 flex-column"
+							style={{ display: 'flex', flex: 'auto' }}
+						>
+							<Link to="" style={{ maxWidth: '100px', alignSelf:'center', marginTop:10 }}>
+								<img
+									src="https://is1-ssl.mzstatic.com/image/thumb/Podcasts123/v4/6b/06/fd/6b06fdbd-07b0-032e-7f4d-ea7c40995278/mza_3169076889313867971.jpg/400x400bb.jpg"
+									style={{ maxHeight: '100px', maxWidth: '100px' }}
+									className="img-thumb"
+									alt="#"
+								/>
+							</Link>
+							<div style={{ flex: 1, padding: '5px 10px', alignSelf:'center' }}>
+								<Link to="#" style={{textAlign:'center'}}>
+									<p
+										style={{
+											fontSize: '1rem',
+											marginBottom: 0,
+											fontFamily: 'inherit',
+											fontWeight: 500,
+											lineHeight: 1.2
+										}}
+									>
+										Nerdcast
+									</p>
+								</Link>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+							</div>
+						</div>
+
+
+
+
+
+
+
+
+
+
+						<div
+							className="custom-card mx-5 mb-3 flex-column"
+							style={{ display: 'flex', flex: 'auto' }}
+						>
+							<Link to="" style={{ maxWidth: '100px', alignSelf:'center', marginTop:10 }}>
+								<img
+									src="https://is1-ssl.mzstatic.com/image/thumb/Podcasts123/v4/6b/06/fd/6b06fdbd-07b0-032e-7f4d-ea7c40995278/mza_3169076889313867971.jpg/400x400bb.jpg"
+									style={{ maxHeight: '100px', maxWidth: '100px' }}
+									className="img-thumb"
+									alt="#"
+								/>
+							</Link>
+							<div style={{ flex: 1, padding: '5px 10px', alignSelf:'center' }}>
+								<Link to="#" style={{textAlign:'center'}}>
+									<p
+										style={{
+											fontSize: '1rem',
+											marginBottom: 0,
+											fontFamily: 'inherit',
+											fontWeight: 500,
+											lineHeight: 1.2
+										}}
+									>
+										Nerdcast
+									</p>
+								</Link>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+							</div>
+						</div>
+
+
+
+
+
+
+
+
+
+
+						<div
+							className="custom-card mx-5 mb-3 flex-column"
+							style={{display: 'flex', flex: 'auto'}}
+						>
+							<Link to="" style={{ maxWidth: '100px', alignSelf:'center', marginTop:10 }}>
+								<img
+									src="https://is1-ssl.mzstatic.com/image/thumb/Podcasts123/v4/6b/06/fd/6b06fdbd-07b0-032e-7f4d-ea7c40995278/mza_3169076889313867971.jpg/400x400bb.jpg"
+									style={{ maxHeight: '100px', maxWidth: '100px' }}
+									className="img-thumb"
+									alt="#"
+								/>
+							</Link>
+							<div style={{ flex: 1, padding: '5px 10px', alignSelf:'center' }}>
+								<Link to="#" style={{textAlign:'center'}}>
+									<p
+										style={{
+											fontSize: '1rem',
+											marginBottom: 0,
+											fontFamily: 'inherit',
+											fontWeight: 500,
+											lineHeight: 1.2
+										}}
+									>
+										Nerdcast
+									</p>
+								</Link>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+								<span className="badge bg-green m-2">Filmes</span>
+							</div>
+						</div>
+
+
+
+
+
+
+						<div
+							className="custom-card mx-5 mb-3 flex-column"
+							style={{display: 'flex', flex: 'auto' }}
 						>
 							<Link to="" style={{ maxWidth: '100px', alignSelf:'center', marginTop:10 }}>
 								<img
