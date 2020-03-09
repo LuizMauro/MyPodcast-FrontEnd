181928d5eb2a61bfb402525366459c7a7c2ee012
Matheus Vilela <37050275+matigous@users.noreply.github.com>
2020-03-09 13:42:20 -0300

    Update index.js

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -68,88 +68,7 @@ export default function Dashboard() {
                         </Card>
                     </Col>
                     <Col lg="3" md="6" sm="6">
-                        <Card className="card-stats">
-                            <CardBody>
-                                <Row>
-                                    <Col md="4" xs="5">
-                                        <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-money-coins text-success" />
-                                        </div>
-                                    </Col>
-                                    <Col md="8" xs="7">
-                                        <div className="numbers">
-                                            <p className="card-category">Revenue</p>
-                                            <CardTitle tag="p">$ 1,345</CardTitle>
-                                            <p />
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </CardBody>
-                            <CardFooter>
-                                <hr />
-                                <div className="stats">
-                                    <i className="far fa-calendar" /> Last day
-                  </div>
-                            </CardFooter>
-                        </Card>
-                    </Col>
-                    <Col lg="3" md="6" sm="6">
-                        <Card className="card-stats">
-                            <CardBody>
-                                <Row>
-                                    <Col md="4" xs="5">
-                                        <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-vector text-danger" />
-                                        </div>
-                                    </Col>
-                                    <Col md="8" xs="7">
-                                        <div className="numbers">
-                                            <p className="card-category">Errors</p>
-                                            <CardTitle tag="p">23</CardTitle>
-                                            <p />
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </CardBody>
-                            <CardFooter>
-                                <hr />
-                                <div className="stats">
-                                    <i className="far fa-clock" /> In the last hour
-                  </div>
-                            </CardFooter>
-                        </Card>
-                    </Col>
-                    <Col lg="3" md="6" sm="6">
-                        <Card className="card-stats">
-                            <CardBody>
-                                <Row>
-                                    <Col md="4" xs="5">
-                                        <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-favourite-28 text-primary" />
-                                        </div>
-                                    </Col>
-                                    <Col md="8" xs="7">
-                                        <div className="numbers">
-                                            <p className="card-category">Followers</p>
-                                            <CardTitle tag="p">+45K</CardTitle>
-                                            <p />
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </CardBody>
-                            <CardFooter>
-                                <hr />
-                                <div className="stats">
-                                    <i className="fas fa-sync-alt" /> Update now
-                  </div>
-                            </CardFooter>
-                        </Card>
-                    </Col>
-                </Row>
-
-                <Row className="p-50 m-50">
-                    <Col lg="3" md="6" sm="6">
-                        <Card className="card-stats">
+                        <Card className="card-stats .bg-dark">
                             <CardBody>
                                 <Row>
                                     <Col md="4" xs="5">
@@ -175,18 +94,18 @@ export default function Dashboard() {
                         </Card>
                     </Col>
                     <Col lg="3" md="6" sm="6">
-                        <Card className="card-stats">
+                        <Card className="card-stats .bg-dark">
                             <CardBody>
                                 <Row>
                                     <Col md="4" xs="5">
                                         <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-money-coins text-success" />
+                                            <i className="nc-icon nc-globe text-warning" />
                                         </div>
                                     </Col>
                                     <Col md="8" xs="7">
                                         <div className="numbers">
-                                            <p className="card-category">Revenue</p>
-                                            <CardTitle tag="p">$ 1,345</CardTitle>
+                                            <p className="card-category">Capacity</p>
+                                            <CardTitle tag="p">150GB</CardTitle>
                                             <p />
                                         </div>
                                     </Col>
@@ -195,24 +114,24 @@ export default function Dashboard() {
                             <CardFooter>
                                 <hr />
                                 <div className="stats">
-                                    <i className="far fa-calendar" /> Last day
+                                    <i className="fas fa-sync-alt" /> Update Now
                   </div>
                             </CardFooter>
                         </Card>
                     </Col>
                     <Col lg="3" md="6" sm="6">
-                        <Card className="card-stats">
+                        <Card className="card-stats .bg-dark">
                             <CardBody>
                                 <Row>
                                     <Col md="4" xs="5">
                                         <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-vector text-danger" />
+                                            <i className="nc-icon nc-globe text-warning" />
                                         </div>
                                     </Col>
                                     <Col md="8" xs="7">
                                         <div className="numbers">
-                                            <p className="card-category">Errors</p>
-                                            <CardTitle tag="p">23</CardTitle>
+                                            <p className="card-category">Capacity</p>
+                                            <CardTitle tag="p">150GB</CardTitle>
                                             <p />
                                         </div>
                                     </Col>
@@ -221,24 +140,53 @@ export default function Dashboard() {
                             <CardFooter>
                                 <hr />
                                 <div className="stats">
-                                    <i className="far fa-clock" /> In the last hour
+                                    <i className="fas fa-sync-alt" /> Update Now
+                  </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                </Row>
+
+                <Row className="p-50 m-50">
+                    <Col lg="3" md="6" sm="6">
+                        <Card className="card-stats .bg-dark">
+                            <CardBody>
+                                <Row>
+                                    <Col md="4" xs="5">
+                                        <div className="icon-big text-center icon-warning">
+                                            <i className="nc-icon nc-globe text-warning" />
+                                        </div>
+                                    </Col>
+                                    <Col md="8" xs="7">
+                                        <div className="numbers">
+                                            <p className="card-category">Capacity</p>
+                                            <CardTitle tag="p">150GB</CardTitle>
+                                            <p />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="stats">
+                                    <i className="fas fa-sync-alt" /> Update Now
                   </div>
                             </CardFooter>
                         </Card>
                     </Col>
                     <Col lg="3" md="6" sm="6">
-                        <Card className="card-stats">
+                        <Card className="card-stats .bg-dark">
                             <CardBody>
                                 <Row>
                                     <Col md="4" xs="5">
                                         <div className="icon-big text-center icon-warning">
-                                            <i className="nc-icon nc-favourite-28 text-primary" />
+                                            <i className="nc-icon nc-globe text-warning" />
                                         </div>
                                     </Col>
                                     <Col md="8" xs="7">
                                         <div className="numbers">
-                                            <p className="card-category">Followers</p>
-                                            <CardTitle tag="p">+45K</CardTitle>
+                                            <p className="card-category">Capacity</p>
+                                            <CardTitle tag="p">150GB</CardTitle>
                                             <p />
                                         </div>
                                     </Col>
@@ -247,7 +195,59 @@ export default function Dashboard() {
                             <CardFooter>
                                 <hr />
                                 <div className="stats">
-                                    <i className="fas fa-sync-alt" /> Update now
+                                    <i className="fas fa-sync-alt" /> Update Now
+                  </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col lg="3" md="6" sm="6">
+                        <Card className="card-stats .bg-dark">
+                            <CardBody>
+                                <Row>
+                                    <Col md="4" xs="5">
+                                        <div className="icon-big text-center icon-warning">
+                                            <i className="nc-icon nc-globe text-warning" />
+                                        </div>
+                                    </Col>
+                                    <Col md="8" xs="7">
+                                        <div className="numbers">
+                                            <p className="card-category">Capacity</p>
+                                            <CardTitle tag="p">150GB</CardTitle>
+                                            <p />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="stats">
+                                    <i className="fas fa-sync-alt" /> Update Now
+                  </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                    <Col lg="3" md="6" sm="6">
+                        <Card className="card-stats .bg-dark">
+                            <CardBody>
+                                <Row>
+                                    <Col md="4" xs="5">
+                                        <div className="icon-big text-center icon-warning">
+                                            <i className="nc-icon nc-globe text-warning" />
+                                        </div>
+                                    </Col>
+                                    <Col md="8" xs="7">
+                                        <div className="numbers">
+                                            <p className="card-category">Capacity</p>
+                                            <CardTitle tag="p">150GB</CardTitle>
+                                            <p />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="stats">
+                                    <i className="fas fa-sync-alt" /> Update Now
                   </div>
                             </CardFooter>
                         </Card>
