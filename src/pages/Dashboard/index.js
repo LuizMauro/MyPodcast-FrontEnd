ff000352c70252c9e3911e948279972e84e3963d
Matheus Vilela <37050275+matigous@users.noreply.github.com>
2020-03-10 13:50:37 -0300

    Update index.js

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -7,7 +7,7 @@ import {
   BrowserRouter as Router,
   Route,
 } from "react-router-dom";
-import Navbar from './components/Navbar';
+import Navbar from './components/NavbarDash/Navbar';
 
 
 import api from '../../services/api'
