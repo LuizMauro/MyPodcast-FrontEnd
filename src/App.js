6632a56d78f4e36cf2dc58959ceef870cad568cb
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-04-04 21:57:36 -0300

    fix edit categoria

--- src/App.js
+++ src/App.js
@@ -7,38 +7,13 @@ import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
-<<<<<<< HEAD
-import history from './services/history'
-import SideBar from './sidebar';
-
-import { store, persistor } from './store'
-
-import GlobalStyle from './styles/global'
-=======
 import history from './services/history';
->>>>>>> master
 
 import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <Provider store={store}>
-
-      <PersistGate persistor={persistor}>
-        <Router history={history}>
-          <Routes />
-        </Router>
-        <GlobalStyle />
-        <SideBar />
-        <ToastContainer autoClose={3000} />
-      </PersistGate>
- 
-    </Provider>
-  );
-=======
 	return (
 		<Provider store={store}>
 			<PersistGate persistor={persistor}>
@@ -50,7 +25,6 @@ function App() {
 			</PersistGate>
 		</Provider>
 	);
->>>>>>> master
 }
 
 export default App;
