ff1e22ba47404c530ad25df00e29ac8e25f7a9f3
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-04-23 15:10:40 -0300

    fix req route adm/mod

--- src/store/modules/categoria/sagas.js
+++ src/store/modules/categoria/sagas.js
@@ -1,45 +1,45 @@
-import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from "redux-saga/effects";
 
-import api from '../../../services/api';
-import history from '../../../services/history';
-import { toast } from 'react-toastify';
+import api from "../../../services/api";
+import history from "../../../services/history";
+import { toast } from "react-toastify";
 
 export function* createCategoria({ payload }) {
-	try {
-		const { ctg_descricao } = payload;
+  try {
+    const { ctg_descricao } = payload;
 
-		const response = yield call(api.post, '/categoria', {
-			ctg_descricao,
-		});
+    const response = yield call(api.post, "adm/categoria", {
+      ctg_descricao,
+    });
 
-		if (response.data.ctgExists) {
-			toast.error('Categoria já cadastrada!');
-		} else if (response.data.ctgCreated) {
-			toast.success('Categoria cadastrada!');
-			history.push('../categorias');
-		} else {
-			toast.error('Falha no cadastro');
-		}
-	} catch (err) {
-		toast.error('Erro ao cadastrar categoria!');
-		console.tron.log(err);
-	}
+    if (response.data.ctgExists) {
+      toast.error("Categoria já cadastrada!");
+    } else if (response.data.ctgCreated) {
+      toast.success("Categoria cadastrada!");
+      history.push("../categorias");
+    } else {
+      toast.error("Falha no cadastro");
+    }
+  } catch (err) {
+    toast.error("Erro ao cadastrar categoria!");
+    console.tron.log(err);
+  }
 }
 
 export function* updateCategoria({ payload }) {
-	try {
-		const { ctg_descricao, ctg_id } = payload;
+  try {
+    const { ctg_descricao, ctg_id } = payload;
 
-		yield call(api.put, `/categoria/${ctg_id}`, {
-			ctg_descricao,
-		});
-		toast.success('Categoria editada');
-	} catch (err) {
-		toast.error('Erro ao atualizar categoria');
-	}
+    yield call(api.put, `/adm/categoria/${ctg_id}`, {
+      ctg_descricao,
+    });
+    toast.success("Categoria editada");
+  } catch (err) {
+    toast.error("Erro ao atualizar categoria");
+  }
 }
 
 export default all([
-	takeLatest('@ctg/CREATE_CATEGORIA_REQUEST', createCategoria),
-	takeLatest('@ctg/UPDATE_CATEGORIA_REQUEST', updateCategoria),
+  takeLatest("@ctg/CREATE_CATEGORIA_REQUEST", createCategoria),
+  takeLatest("@ctg/UPDATE_CATEGORIA_REQUEST", updateCategoria),
 ]);
