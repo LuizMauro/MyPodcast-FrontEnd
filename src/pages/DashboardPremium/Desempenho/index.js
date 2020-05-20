e9e55245be61c1db1bcf45f462dc69a9547f67c8
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-05-20 16:16:45 -0300

    dash premium desempenho v1

--- src/pages/DashboardPremium/Desempenho/index.js
+++ src/pages/DashboardPremium/Desempenho/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Form } from "@unform/web";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
-
+import api from "../../../services/api";
 import { FaPen, FaTimes, FaPlus } from "react-icons/fa";
 import { MdClose } from "react-icons/md";
 
@@ -18,10 +18,36 @@ import {
 } from "reactstrap";
 
 export default function EditarPodcast() {
-  useEffect(() => {}, []);
+  const [podcasts, setPodcasts] = useState([]);
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    let podinicial = null;
+
+    async function exibirPodcasts() {
+      const response = await api.get("/userpodcasts");
+      setPodcasts(response.data);
+      if (response.data.length > 0) {
+        podinicial = response.data[0].pod_id;
+        loadEstatistica();
+      }
+    }
+
+    async function loadEstatistica() {
+      const response = await api.get(`/estatisticaspremium/${podinicial}`);
+      setDados(response.data);
+    }
+
+    exibirPodcasts();
+    loadEstatistica();
+  }, []);
+
+  async function SelecionarPodcast() {}
 
   return (
     <>
+      {console.log("dados", dados)}
+      {console.log("pods", podcasts)}
       <section className="section section-shaped section-lg">
         <Container className="pt-lg-1">
           <Row style={{ justifyContent: "center" }}>
@@ -33,7 +59,39 @@ export default function EditarPodcast() {
                 >
                   <CardTitle
                     style={{ fontSize: 25, color: "#fff", marginTop: 20 }}
-                  ></CardTitle>
+                  >
+                    Desempenho
+                  </CardTitle>
+                  <Row>
+                    <Col lg="6">
+                      <select
+                        className="select-home shadow"
+                        onChange={SelecionarPodcast}
+                        value={podcasts}
+                        style={{ color: "#fff", width: "100%" }}
+                        type="select"
+                        name="select"
+                        id="exampleSelect"
+                      >
+                        {podcasts.map((item) => (
+                          <option value={item.pod_id}>{item.pod_nome}</option>
+                        ))}
+                      </select>
+                    </Col>
+                  </Row>
+                  <Row className="mt-3">
+                    <Col lg="3" sm="6">ABC</Col>
+                    <Col lg="3" sm="6">DEF</Col>
+                    <Col lg="3" sm="6">GHI</Col>
+                    <Col lg="3" sm="6">JKL</Col>
+                  </Row>
+                  <Row className="mt-3">
+                    <Col lg="6" md="12">ABC</Col>
+                    <Col lg="6" md="12">ABC</Col>
+                  </Row>
+                  <Row className="mt-3">
+                    <Col lg="12">123</Col>
+                  </Row>
                 </CardBody>
               </Card>
             </Col>
